db7f5fd4baf5f858afba3d0bbff26c8317992fbc
Thomas Witting <48446458+witting97@users.noreply.github.com>
2022-05-10 17:01:09 +0200

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,13 @@
+# This shell script runs when the machine starts, 
+# before the user-installed packages are set up.
+#
+# Use it to install additional dependencies.
+#
+# Examples:
+#
+# sudo apt update && sudo apt install -y some-package
+#
+# pip install some-package --additional-arguments
+#
+# The logs are stored in /tmp/log;
+# the error log is copied to Notebook files.
